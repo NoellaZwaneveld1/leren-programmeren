2d422d91a224a4a50ac5124339c2e2c893b78c7b
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2022-09-08 11:52:46 +0200

    nog een aanpassing Speelhal!

--- speelhal.py
+++ speelhal.py
@@ -3,7 +3,7 @@ toegangsticketPerPersoon = toegangsticketPerPersoon * 100
 aantalPersonen = int(input('Met hoeveel personen ben je? '))  #4
 toegangTotaal = toegangsticketPerPersoon * aantalPersonen #29.8 euro in totaal
 
-vipSeatTijd = int(input('Hoelang wil je in de VR-vip seat? '))  #45
+vipSeatTijd = int(input('Hoeveel minuten wil je in de VR-vip seat? '))  #45
 vipSeatPrijs = float (input('Wat is de prijs per 5 minuten? '))#0.37 per 5 minuten 
 vipSeatPrijs = vipSeatPrijs * 100
 vipseatPerPersoon = vipSeatTijd / 5 * vipSeatPrijs
