d3b8eaee6d6409e3a6b5f74c9fe0e4caebf01c7d
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2022-09-08 11:50:09 +0200

    speelhal en feestlunch in centen!!!

--- feestlunch.py
+++ feestlunch.py
@@ -1,15 +1,18 @@
-aantalCroissantjes = 17
-prijsPerCroissantje = 0.39
+aantalCroissantjes = int(input('Hoeveel croissantjes wilt u kopen? ')) #17 croissantjes
+prijsPerCroissantje = float(input('Wat is de prijs per croissantje? ')) #0.39 
+prijsPerCroissantje = prijsPerCroissantje * 100
 croissantjes = aantalCroissantjes * prijsPerCroissantje
 
-aantalStokbroden = 2
-prijsPerStokbroden = 2.78
+aantalStokbroden = int(input('Hoeveel stokbroden wilt u kopen? ')) #2 stokbroden
+prijsPerStokbroden = float(input('Wat is de prijs per stokbrood?')) #2.78
+prijsPerStokbroden = prijsPerStokbroden * 100
 stokbroden = aantalStokbroden * prijsPerStokbroden
 
-aantalKortingsbonnen = 3
-kortingPerBon = 0.50
+aantalKortingsbonnen = int(input('Hoeveel kortingsbonnen heeft u?')) #3
+kortingPerBon = float(input('Hoeveel korting per kortingsbon? ')) #0.50
+kortingPerBon = kortingPerBon *100
 kortingsbonnen = aantalKortingsbonnen * kortingPerBon
 
 totaalPrijs = croissantjes + stokbroden - kortingsbonnen
 
-print('De feestlunch kost je bij de bakker', totaalPrijs, 'voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
+print('De feestlunch kost je bij de bakker', totaalPrijs, ' cent voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig')
--- speelhal.py
+++ speelhal.py
@@ -1,12 +1,14 @@
-toegangsticketPerPersoon = 7.45
-aantalPersonen = 4
-toegangTotaal = toegangsticketPerPersoon * aantalPersonen
+toegangsticketPerPersoon = float(input('Hoeveel kost een ticket per persoon? ')) #7.45
+toegangsticketPerPersoon = toegangsticketPerPersoon * 100 
+aantalPersonen = int(input('Met hoeveel personen ben je? '))  #4
+toegangTotaal = toegangsticketPerPersoon * aantalPersonen #29.8 euro in totaal
 
-vipSeatTijd = 45 / 5
-vipSeatPrijs = 0.37 
-vipseatPerPersoon = vipSeatTijd * vipSeatPrijs
-vipseatTotaal = vipseatPerPersoon * aantalPersonen
+vipSeatTijd = int(input('Hoelang wil je in de VR-vip seat? '))  #45
+vipSeatPrijs = float (input('Wat is de prijs per 5 minuten? '))#0.37 per 5 minuten 
+vipSeatPrijs = vipSeatPrijs * 100
+vipseatPerPersoon = vipSeatTijd / 5 * vipSeatPrijs
+vipseatTotaal = vipseatPerPersoon * aantalPersonen #13.32 in totaal
 
 totaalPrijs = toegangTotaal + vipseatTotaal 
 
-print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar', totaalPrijs, 'euro')
+print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar', totaalPrijs, 'cent')
