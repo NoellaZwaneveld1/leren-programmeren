1440c608bd2009cb6506c37fc1367a71d51e3074
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2022-09-09 09:47:04 +0200

    Min en Max

--- module2/metenIsWeten.py
+++ module2/metenIsWeten.py
@@ -2,12 +2,26 @@ a = input('Welk getal is a? ')
 b = input('Welk getal is b? ')
 
 if a > b:
+    min = min(b)
     max = max(a)
     print(f'A is het grootste getal: {max}')
+    
+    print(f"""
+    Het minimum is:  {min}
+    Het maximum is:  {max}    
+    """)
 
 elif a < b:
     min = min(a)
+    max = max(b)
     print(f'A is het kleinste getal: {min}')
+
+    print(f"""
+    Het minimum is:  {min}
+    Het maximum is:  {max}    
+    """)
 
 else:
     print('A en b zijn even groot')
+
+   
