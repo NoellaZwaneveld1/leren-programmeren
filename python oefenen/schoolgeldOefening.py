af19a42b02ab8c07543083bfb7b89c81660005bb
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2022-09-09 09:21:04 +0200

    schoolgeld.py versie 2

---
+++ python oefenen/schoolgeldOefening.py
@@ -0,0 +1,16 @@
+schoolgeld = 1239
+aantalStudenten = 24
+
+collegegeld = schoolgeld * aantalStudenten
+
+print(f'Het college geld voor deze klas is dit jaar {collegegeld} euro')
+
+
+btw = 9
+btwAppels = 3.40 / 100 * btw
+btwDruiven = 2.45 / 100 * btw
+btwBanen = 1.95 / 100 * btw 
+
+btwTotaal = btwAppels + btwDruiven + btwBanen
+
+print(f'Totaal btw is: {btwTotaal} euro')
