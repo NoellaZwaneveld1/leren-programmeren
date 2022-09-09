447ef31d448fad9f3aed2b1b851c15aaec0add22
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2022-09-09 09:41:20 +0200

    elif-statement

--- module2/metenIsWeten.py
+++ module2/metenIsWeten.py
@@ -4,3 +4,7 @@ b = input('Welk getal is b? ')
 if a > b:
     max = max(a)
     print(f'A is het grootste getal: {max}')
+
+elif a < b:
+    min = min(a)
+    print(f'A is het kleinste getal: {min}')
