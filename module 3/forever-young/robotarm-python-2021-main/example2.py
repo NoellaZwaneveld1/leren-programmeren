95726a7caa1a306bb68c733134fbcc5348a3df29
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2022-10-05 09:20:31 +0200

    opdracht 3 robotarm

--- module 3/forever-young/robotarm-python-2021-main/example2.py
+++ module 3/forever-young/robotarm-python-2021-main/example2.py
@@ -4,28 +4,28 @@ robotArm = RobotArm('exercise 2')
 
 robotArm.grab()
 
-for x in range(1,10):
+for x in range(1,10): #9 keer naar rechts
     robotArm.moveRight()
 
 robotArm.drop()
 
 for x in range (9, 7, -1):
-    robotArm.moveLeft()
+    robotArm.moveLeft() #2 keer naar links
 
 robotArm.grab()
 
 for x in range (7, 9):
-    robotArm.moveRight()
+    robotArm.moveRight() #2 keer naar rechts
 
 robotArm.drop()
 
 for x in range(9, 4, -1):
-    robotArm.moveLeft()
+    robotArm.moveLeft() #5 keer links
 
 robotArm.grab()
 
 for x in range(4, 9):
-    robotArm.moveRight()
+    robotArm.moveRight() #5 keer rechts
 
 robotArm.drop()
 robotArm.wait()
