4af383bdc19e9a7eec2e94e2665366b6e94c3786
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2022-10-08 15:57:20 +0200

    blastoff opdracht 2

--- module 3/Deel1_voorAllesInDeVoorraad/blastoff.py
+++ module 3/Deel1_voorAllesInDeVoorraad/blastoff.py
@@ -1,7 +1,16 @@
-startLancering = 30
-lancering = range(30, -1, -1) #telt terug vanaf 30 met een stap van 1
+# startLancering = 30
+# lancering = range(30, -1, -1) #telt terug vanaf 30 met een stap van 1
 
-for startLancering in lancering:
-    print (startLancering)
-    if startLancering <= 0:
-        print ('De raket is gelanceerd')
+# for startLancering in lancering:
+#     print (startLancering)
+#     if startLancering <= 0:
+#         print ('De raket is gelanceerd')
+
+startLancering = 30
+
+while startLancering > 0:
+    print(startLancering)
+    startLancering = startLancering - 1
+
+if startLancering == 0:
+    print('De raket is gelanceerd')
