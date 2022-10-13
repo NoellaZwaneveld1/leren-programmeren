d9f25432d0e63de1cbc7c6c9fd077693022428b6
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2022-10-13 09:28:49 +0200

    Challenge Zwembad

---
+++ module 3/Deel1_voorAllesInDeVoorraad/zwembad.py
@@ -0,0 +1,7 @@
+lengte = 8
+breedte = 3
+hoogte = 1.5
+
+inhoudZwembad = round(lengte * breedte * hoogte, 2)
+
+print (inhoudZwembad)
---
+++ python oefenen/oefening1.py
@@ -0,0 +1,19 @@
+#oefening 1
+# name = input('What is your name? ')
+# age = int(input('How old are you? '))
+
+# currentYear = 2022
+
+# hundredYearsOld = currentYear - age + 100
+
+# print (f"""
+# Hello {name}, you're now {age} years old.
+# You will be 100 years old in {hundredYearsOld}
+# """)
+
+number = int(input("NUMBER: "))
+
+if number % 2 == 0:
+    print(f"{number},given number is even!")
+else:
+    print(f"{number},given number is odd")
