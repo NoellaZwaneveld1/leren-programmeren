cf1f7da223a1ae961a9b20b5ccebbac067190898
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2022-10-13 09:33:44 +0200

    challenge part 2

--- module 3/Deel1_voorAllesInDeVoorraad/zwembad.py
+++ module 3/Deel1_voorAllesInDeVoorraad/zwembad.py
@@ -4,4 +4,16 @@ hoogte = 1.5
 
 inhoudZwembad = round(lengte * breedte * hoogte, 2)
 
-print (inhoudZwembad)
+uitgraven = 25
+uitgravenPrijs = uitgraven * inhoudZwembad
+
+afvoeren = 32.50 
+afvoerenPrijs = afvoeren * inhoudZwembad
+
+totaal = round(uitgravenPrijs + afvoerenPrijs, 2) 
+
+print(f"""Offerte voor een zwembad van 8 bij 3 bij 1,5 meter (inhoud: {inhoudZwembad} m3)
+Uitgraven:         €{uitgravenPrijs}
+Afvoeren grond:    €{afvoerenPrijs}
+Totaal:            €{totaal}
+""")
