905eec851d48e69591eaff2c3457dfc07144df2f
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2023-03-15 11:47:19 +0100

    person.py new

---
+++ module2/Deel2_userinput/personNew.py
@@ -0,0 +1,13 @@
+naam = input("Wat is je naam? ")
+adres = input("Wat is je adres? ")
+postcode = input("Wat is je postcode? ")
+woonplaats = input("Wat is je woonplaats? ") 
+ 
+print(f""" 
+    ----------------------------------------------------
+    |  Naam      : {naam}               
+    |  Adres     : {adres}          
+    |  Postcode  : {postcode}                         
+    |  Woonplaats: {woonplaats}    
+    ----------------------------------------------------
+""")
