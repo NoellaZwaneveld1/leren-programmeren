0a408af66f3298b0b1f5b8f0f12149663221340d
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2023-03-29 11:05:22 +0200

    blastoff new

---
+++ module 3/Deel1_voorAllesInDeVoorraad/blastoffNew.py
@@ -0,0 +1,9 @@
+startlancering = 30
+
+lancering = range(30, -1, -1)
+
+for startlancering in lancering:
+    print(startlancering)
+    
+    if  startlancering == 0: 
+        print ('De raket is gelanceerd')
