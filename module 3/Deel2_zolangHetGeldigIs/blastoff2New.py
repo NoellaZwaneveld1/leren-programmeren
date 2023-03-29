ee11b45c222601d7753b232d5f34fdc1bb66551d
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2023-03-29 12:10:57 +0200

    blastoff 2 new

---
+++ module 3/Deel2_zolangHetGeldigIs/blastoff2New.py
@@ -0,0 +1,7 @@
+startlancering = 30
+
+while startlancering >0:
+    print(startlancering)
+    startlancering = startlancering -1
+    if startlancering == 0:
+        print("De raket is gelanceerd")
