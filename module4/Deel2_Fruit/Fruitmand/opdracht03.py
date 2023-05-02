787459ea5bdb2922158482fa652c6e2acf890739
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2023-05-02 14:32:02 +0200

    opdracht 6

--- module4/Deel2_Fruit/Fruitmand/opdracht03.py
+++ module4/Deel2_Fruit/Fruitmand/opdracht03.py
@@ -1,5 +1,8 @@
 from fruitmand import fruitmand 
 
-for fruitnaam in range(0, len(fruitmand)):
-    print(fruitmand[fruitnaam].get("name"))
+for fruit in fruitmand:
+    print(fruit['name'])
+    
+# for positie in range(0, len(fruitmand)):
+#     print(fruitmand[positie].get("name"))
 
--- module4/Deel2_Fruit/Fruitmand/opdracht04.py
+++ module4/Deel2_Fruit/Fruitmand/opdracht04.py
@@ -4,5 +4,5 @@ import random
 getal = int(input("Vul een getal in "))
 
 for aantalFruit in range(0, getal):
-    randomFruit = random.choice(fruitmand).get("name")
-    print(randomFruit)
+    randomFruit = random.choice(fruitmand)
+    print(randomFruit['name'])
