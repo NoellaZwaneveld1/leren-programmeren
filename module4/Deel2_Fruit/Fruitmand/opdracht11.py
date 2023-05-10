d2be1b3d6418512a783bccd7ad976be686b4f9ac
NoellaZwaneveld1 <113012099+NoellaZwaneveld1@users.noreply.github.com>
2023-05-10 12:08:06 +0200

    fruitmand opdracht 11

---
+++ module4/Deel2_Fruit/Fruitmand/opdracht11.py
@@ -0,0 +1,33 @@
+from fruitmand import fruitmand
+
+fruitKleuren = [] #lege list
+
+for kleuren in fruitmand:
+    fruitKleuren.append(kleuren['color']) #zet alle kleuren in de lege list
+
+while True:
+    welkeKleur = input("Welke kleur is het fruit? ")
+    if welkeKleur in fruitKleuren:
+        break
+    else:
+        print("Vul een kleur in die in de fruitmand zit")
+
+rond = 0
+niet_rond = 0
+
+for fruit in fruitmand:
+    alleKleuren = fruit['color']
+    if alleKleuren == welkeKleur:
+        if fruit['round']:
+            rond+=1
+        else:
+            niet_rond +=1
+
+verschil = abs(rond - niet_rond)
+
+if rond > niet_rond:
+    print(f"Er zijn {verschil} meer ronde vruchten dan niet ronde vruchten van de kleur {welkeKleur}")
+if rond < niet_rond:
+    print(f"Er zijn {verschil} meer niet ronde vruchten dan ronde vruchten van de kleur {welkeKleur}")
+else: 
+    print(f"Er zijn evenveel ronde als niet ronde vruchten van de kleur {welkeKleur}")
